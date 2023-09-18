f72a87687ab034e143382a4ec38ad6820c78d4fe
Varun Gaur <95574483+Vx1o7@users.noreply.github.com>
2023-09-18 17:48:24 +0530

    Update COMPRESSVD.java

--- Codechef Problems/09.18.2023 Second Shift/COMPRESSVD.java
+++ Codechef Problems/09.18.2023 Second Shift/COMPRESSVD.java
@@ -1,1 +1,34 @@
+https://www.codechef.com/problems/COMPRESSVD
 
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t!=0){
+		   int n=sc.nextInt(); 
+		   int a[]=new int[n];
+		   for(int i=0;i<n;i++){
+		       a[i]=sc.nextInt();
+		   }
+		   int c=n;
+		   for(int i=0;i<n-1;i++){
+		       if(a[i]==a[i+1]){
+		           c--;
+		       }
+		   }
+		   System.out.println(c);
+		   
+		    t--;
+		
+		}
+	}
+}
