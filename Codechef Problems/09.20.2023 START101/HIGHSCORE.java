9aa714cfa065e01edd6c276ced7167671a79a850
Varun Gaur <95574483+Vx1o7@users.noreply.github.com>
2023-09-20 12:06:01 +0530

    Create HIGHSCORE.java

---
+++ Codechef Problems/09.20.2023 START101/HIGHSCORE.java
@@ -0,0 +1,26 @@
+https://www.codechef.com/problems/HIGHSCORE
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner (System.in);
+	    int t = sc.nextInt();
+	    while (t>0) {
+	        int n=sc.nextInt();
+		        int a=sc.nextInt();
+		        int b=sc.nextInt();
+		        int c=sc.nextInt();
+		        int d=sc.nextInt();
+		        System.out.println(Math.max(a,Math.max(b,Math.max(c,d))));
+	        t--;
+	    }
+	}
+}
