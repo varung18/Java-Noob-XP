ca2cf7f30046ea690cce7321efd50b4c980dc688
Varun Gaur <95574483+Vx1o7@users.noreply.github.com>
2023-09-20 20:03:52 +0530

    Create OFFBY1.java

---
+++ Codechef Problems/09.20.2023 START101/OFFBY1.java
@@ -0,0 +1,21 @@
+https://www.codechef.com/START101D/problems/OFFBY1
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int a = sc.nextInt();
+	    int b = sc.nextInt();
+	    int c = a+b;
+	    System.out.println(c+"1");
+
+	}
+}
